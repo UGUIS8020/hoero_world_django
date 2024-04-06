5c3d4e3ea71dc745f15f48b261fb34977b2bb144
UGUIS8020 <shibuyamasahiko@gmail.com>
2024-04-07 02:15:11 +0900

    新越谷1

--- first_project/first_app/urls.py
+++ first_project/first_app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('page/<str:user_name>',views.user_page, name='user_page'),
     path('number_page/<str:user_name>/<int:number>',views.number_page, name='number_page'),
     path('user_maintenance', views.user_maintenance, name='user_maintenance'),
+    path('my-view/', views.my_view, name='my-view'),
 ]
--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
 
 def index(request):
     return render(request, 'index.html')
@@ -13,6 +14,11 @@ def user_page(request, user_name):
 def number_page(request, user_name, number):
     user_name = user_name.upper()
     return HttpResponse(f'<h1>{user_name}\'s page number = {number}</h1>')
+
+def my_view(request):
+    # メッセージを追加
+    messages.add_message(request, messages.INFO, 'Your message here.')
+    return render(request, 'my_template.html')
 
 
 
